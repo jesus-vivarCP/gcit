f182b1623dd27295292316ffacd7dd291b3b89c0
jesusvivar <jesusvivar@192.168.0.44>
2017-04-27 15:34:56 -0400

    Initial commit.

---
+++ LMSWeb/src/com/gcit/lms/web/AdminServlet.java
@@ -0,0 +1,55 @@
+package com.gcit.lms.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gcit.lms.entity.Author;
+import com.gcit.lms.service.AdminService;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+@WebServlet("/addAuthor")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Author author = new Author();
+		author.setAuthorName(request.getParameter("authorName"));
+		AdminService service = new AdminService();
+		try {
+			service.addAuthor(author);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/viewauthors.jsp");
+		rd.forward(request, response);
+	}
+
+}
